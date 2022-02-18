4a8b0fa8f97dbe84b3e6054b8a0c06946d5cc016
Jackw <riotshielder21@gmail.com>
2022-02-18 15:58:13 +0000

    distortion boiler

--- image_manipulation.py
+++ image_manipulation.py
@@ -7,7 +7,8 @@ import numpy as np
 class Image_processes:
 
     def _init_(self):
-        return       
+        self.calibratred =  False
+        return
     
     def runProcessor(self,frame):
         return imageSegmentation(frame)
@@ -45,8 +46,55 @@ class Image_processes:
     def __distortionCorrection(self, images):
         
         #code here to correct lens distortion in 4 images from the camera feed
+        if not self.calibratred:
+            
+            # termination criteria
+            criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+            # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+            objp = np.zeros((6*7,3), np.float32)
+            objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
+            # Arrays to store object points and image points from all the images.
+            objpoints = [] # 3d point in real world spaceeria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+            # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+            objp = np.zeros((6*7,3), np.float32)
+            objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
+            # Arrays to store object points and image points from all the images.
+            objpoints = [] # 3d point in real world space
+            imgpoints = [] # 2d points in image plane.
+            images = glob.glob('*.jpg')
+
+            for fname in images:
+                img = cv.imread(fname)
+                gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+                # Find the chess board corners
+                ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+                # If found, add object points, image points (after refining them)
+                if ret == True:
+                    objpoints.append(objp)
+                    corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+                    imgpoints.append(corners)
+                    # Draw and display the corners
+                    cv.drawChessboardCorners(img, (7,6), corners2, ret)
+                    cv.imshow('img', img)
+                    cv.waitKey(500)
+            cv.destroyAllWindows()
         
+        img = cv.imread('left12.jpg')
+        h,  w = img.shape[:2]
+        newcameramtx, roi = cv.getOptimalNewCameraMa
+        trix(mtx, dist, (w,h), 1, (w,h))
+        # undistort
+        dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+        # crop the image
+        x, y, w, h = roi
+        dst = dst[y:y+h, x:x+w]
+        cv.imwrite('calibresult.png', dst)
+
         return self.imageStitch(images)
+
     
     def __imageStitch(self, images):
     
